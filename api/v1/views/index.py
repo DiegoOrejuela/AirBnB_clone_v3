baa6929959194533ebd26aa0d4324a9c911901c9
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-09-12 04:37:23 +0000

    after pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
 from api.v1.views import app_views
 from flask import jsonify
+'indesx of status'
 
 @app_views.route("/status")
 def status():
+    'returns a json of status'
     return jsonify({"status": "OK"})
