1802b40ae51a4ec364e49dc089216659b3f8c99c
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-09-12 05:49:20 +0000

    T5: retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
+'index of status'
 from api.v1.views import app_views
 from flask import jsonify
-'indesx of status'
+from models import storage
+
 
 @app_views.route("/status")
 def status():
     'returns a json of status'
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """retrieves the number of each objects by type"""
+    dict_classes = {"amenities": "Amenity", "cities": "City",
+                    "places": "Place", "reviews": "Review",
+                    "states": "State", "users": "User"}
+    new_dict = {}
+    for key, value in dict_classes.items():
+        new_dict[key] = storage.count(value)
+    return jsonify(new_dict)
