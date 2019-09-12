afe58a701874996c6feeb6394a87be8522a66d42
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-09-12 06:27:39 +0000

    T6: [feature] add make_response

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """api file with the api flask instance"""
-from flask import Flask, render_template, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -20,7 +20,7 @@ def shutdown_session(exception=None):
 @app.errorhandler(404)
 def not_found(e):
     'error handling'
-    return jsonify({"error": "Not found"})
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
     app.run(host=getenv("HBNB_API_HOST"),
