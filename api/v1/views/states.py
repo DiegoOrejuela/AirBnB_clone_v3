2ff97c722cca0617a25b3289b217aa414bf6f5dc
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-09-12 10:56:18 +0000

    7: Create a new view for State objects that handles all default RestFul API actions

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,22 +1,71 @@
-l#!/usr/bin/python3
+#!/usr/bin/python3
 'states handling for api'
-from flask import jsonify
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.state import State
 
 
-@app.route('/api/v1/states')
-def state():
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def put_state_by_id(state_id):
     'retrive an object into a json'
-    return to_dict(States)
+    if not request.get_json():
+        abort(400, "Not a JSON")
+
+    state = storage.get("State", state_id)
+
+    if state:
+        for key, value in request.get_json().items():
+            if key != "id" and key != "created_at" and key != "updated_at":
+                setattr(state, key, value)
+        state.save()
+    else:
+        abort(404)
+
+    return jsonify(state.to_dict()), 200
 
 
-@app.route('/api/v1/states/:states_id')
-def stateid(states_id):
+@app_views.route('/states', methods=['POST'])
+def post_state_by_id():
     'retrive an object into a json'
-    try:
-        return get(State, states_id)
-    except:
-        raise error 404
+    if not request.get_json():
+        abort(400, "Not a JSON")
+    if not request.get_json().get("name"):
+        abort(400, "Missing name")
+
+    state = State(**request.get_json())
+    state.save()
+    return jsonify(state.to_dict()), 201
 
 
-@app.route('/api/v1/states/<state_id>', methods=['DELETE'])
-def delete
+@app_views.route("/states")
+def states():
+    """retrive an object into a json"""
+    list_states = []
+    for key, value in storage.all("State").items():
+        list_states.append(value.to_dict())
+    return jsonify(list_states)
+
+
+@app_views.route("states/<state_id>")
+def get_state_by_id(state_id):
+    'retrive an object into a json'
+
+    state = storage.get("State", state_id)
+    if state:
+        return jsonify(state.to_dict())
+    else:
+        abort(404)
+
+
+@app_views.route('states/<state_id>', methods=['DELETE'])
+def delete_state_by_id(state_id):
+    'retrive an object into a json'
+
+    state = storage.get("State", state_id)
+    if state:
+        storage.delete(state)
+        storage.save()
+        return jsonify({}), 200
+    else:
+        abort(404)
